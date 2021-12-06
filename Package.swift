d92b5e0dde8a9356da8d2e3eff8f927b87c0afba
JustinMeans <46542161+JustinMeans@users.noreply.github.com>
2021-12-06 00:27:08 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0"),
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.1.0"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
+        .package(url: "https://github.com/JustinMeans/fluent-postgres-driver.git", from: "2.2.2"),
 
         // Well Known Binary Encoding and Decoding
         .package(url: "https://github.com/rabc/WKCodable", from: "0.1.0"),
